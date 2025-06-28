2012f3da2d1f3fb3ad3076544b0ae0c48a000e77
Diogo Bandeira <bandeiradiogo96@gmail.com>
2025-06-27 21:52:44 -0300

    https://github.com/Diogoassun/Projeto-Engenharia-de-Software.git

---
+++ Inc/AFIO_utils.h
@@ -0,0 +1,17 @@
+/*
+ * AFIO_utils.h
+ *
+ *  Created on: Jun 20, 2025
+ *      Author: leand
+ */
+
+#ifndef AFIO_UTILS_H_
+#define AFIO_UTILS_H_
+
+#include "AFIO_registers.h"
+
+#define AFIO_MAPR_SPI1_REMAP		(0x1<<0)
+
+void afio_init_config();
+
+#endif /* AFIO_UTILS_H_ */
